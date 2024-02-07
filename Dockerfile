6d29fba8601a5eff5c4e07aeb321fad997e08140
sidthoviti <sidthoviti@gmail.com>
2024-02-08 03:07:14 +0530

    PT automation framework initial commit. Added basic enumeration tools and created a dockerfile that needs to be tested.

---
+++ Dockerfile
@@ -0,0 +1,69 @@
+# Use Kali Linux as the base image
+FROM kalilinux/kali-rolling
+
+# Install required packages
+RUN apt-get update && apt-get install -y \
+    wget \
+    curl \
+    tar \
+    make \
+    nano \
+    pv \
+    ipcalc \
+    lolcat \
+    git \
+    jq \
+    python3 \
+    python3-pip \
+    whois \
+    nmap \
+    dirsearch \
+    unzip \
+    ccze \
+    libpcap0.8-dev \
+    libuv1-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+# Install Go
+ENV GOLANG_VERSION 1.22.0
+RUN wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz && \
+    tar -C /usr/local -xzf go$GOLANG_VERSION.linux-amd64.tar.gz && \
+    rm go$GOLANG_VERSION.linux-amd64.tar.gz
+
+# Set Go environment variables
+ENV GOPATH="$HOME/go-workspace"
+ENV GOROOT="/usr/local/go"
+ENV PATH="$PATH:$GOROOT/bin/:$GOPATH/bin"
+
+# Clone the autobomb repository
+RUN git clone https://github.com/sidthoviti/autobomb.git /app/autobomb
+
+# Set the working directory
+WORKDIR /app/autobomb
+
+# Make shell scripts executable
+RUN chmod + *.sh
+
+# Install TestSSL
+RUN git clone --depth 1 https://github.com/drwetter/testssl.sh.git && \
+    ln -s /app/autobomb/testssl.sh/testssl.sh /usr/local/bin/testssl.sh
+
+# Install Httpx
+RUN go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+# Install Nuclei
+RUN go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+
+# Install NucleiFuzzer
+RUN git clone https://github.com/0xKayala/NucleiFuzzer.git && \
+    cd NucleiFuzzer && \
+    chmod +x install.sh && \
+    ./install.sh
+
+# Install Subfinder
+RUN go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+# Run updates for tools
+RUN nuclei -up && nuclei
+RUN subfinder -up
+RUN httpx -up
---
+++ autobomb.py
@@ -0,0 +1,113 @@
+import json
+import subprocess
+import os
+import urllib3
+
+#Colors for terminal printing
+RED = "\033[91m"
+BLUE = "\033[94m"
+YELLOW = "\033[93m"
+GREEN = "\033[32m"
+PURPLE = '\033[0;35m' 
+CYAN = "\033[36m"
+END = "\033[0m"
+NC = "\033[0m"  # No Color
+
+banner = f"""
+{RED}       d8888          888            888888b.                          888      
+      d88888          888            888  "88b                         888      
+     d88P888          888            888  .88P                         888      
+    d88P 888 888  888 888888 .d88b.  8888888K.   .d88b.  88888b.d88b.  88888b.  
+   d88P  888 888  888 888   d88""88b 888  "Y88b d88""88b 888 "888 "88b 888 "88b 
+  d88P   888 888  888 888   888  888 888    888 888  888 888  888  888 888  888 
+ d8888888888 Y88b 888 Y88b. Y88..88P 888   d88P Y88..88P 888  888  888 888 d88P 
+d88P     888  "Y88888  "Y888 "Y88P"  8888888P"   "Y88P"  888  888  888 88888P"  
+                                                                                
+                                                                                
+                                                                                
+"""
+print(banner)
+print(f"\n{YELLOW}Penetration Testing Automation Framework \n{GREEN}by Siddharth Thoviti")
+print(f"\n{BLUE}Version: {CYAN}0.1b")
+print(f"\n{BLUE}Info: {CYAN}The script is still in early development stages. Please report any bugs that you may encounter.")
+
+
+# Disable SSL Warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Colors for terminal printing
+YELLOW = "\033[93m"
+NC = "\033[0m"  # No Color
+
+# Read configuration from JSON file
+with open("config.json") as f:
+    config = json.load(f)
+
+# Retrieve URL, Cookie, ProjectName, and LogLocation from config
+url = config.get("URL")
+cookie = config.get("Cookie")
+project_name = config.get("ProjectName")
+log_location = config.get("LogLocation", ".")
+
+if not url:
+    print("Error: URL not provided in the config file.")
+    exit(1)
+
+# Create a directory for the project
+project_directory = os.path.abspath(os.path.join(log_location, project_name))
+os.makedirs(project_directory, exist_ok=True)
+
+# Function to run a command and print status messages
+def run_command(cmd, color, tool_name, log_path):
+    print(f"{color}Running {tool_name}...{NC}")
+    print(f"{cmd}")
+    subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(f"{color}{tool_name} log located at {log_path}{NC}")
+    print(f"Log file content:")
+    subprocess.run(['cat', log_path])
+
+# WebTech
+webtech_log_path = os.path.join(project_directory, 'webtech_' + project_name)
+webtech_cmd = f"webtech -u {url} 2>&1 | tee {webtech_log_path}"
+run_command(webtech_cmd, YELLOW, "WebTech", webtech_log_path)
+
+# Nuclei
+nuclei_log_path = os.path.join(project_directory, "nuclei_" + project_name)
+nuclei_cmd = f"nuclei -u {url} 2>&1 | tee {nuclei_log_path}"
+run_command(nuclei_cmd, YELLOW, "Nuclei", nuclei_log_path)
+
+# NucleiFuzzer
+nf_log_path = os.path.join(project_directory, 'nf_' + project_name)
+nf_cmd = f"nf -d {url} 2>&1 | tee {nf_log_path}"
+run_command(nf_cmd, YELLOW, "NucleiFuzzer", nf_log_path)
+
+# TestSSL
+testssl_log_path = os.path.join(project_directory, 'testssl_' + project_name)
+testssl_cmd = f"/home/sid/tools/testssl.sh/./testssl.sh --warnings off {url} 2>&1 | tee {testssl_log_path}"
+run_command(testssl_cmd, YELLOW, "TestSSL", testssl_log_path)
+
+# Nmap
+nmap_log_path = os.path.join(project_directory, 'nmap_' + project_name)
+nmap_cmd = f"nmap -A -T4 {url} 2>&1 | tee {nmap_log_path}"
+run_command(nmap_cmd, YELLOW, "Nmap", nmap_log_path)
+
+# Wafw00f
+wafw00f_log_path = os.path.join(project_directory, 'wafw00f_' + project_name)
+wafw00f_cmd = f"wafw00f {url} 2>&1 | tee {wafw00f_log_path}"
+run_command(wafw00f_cmd, YELLOW, "Wafw00f", wafw00f_log_path)
+
+# Directory Fuzzing
+ffuf_log_path = os.path.join(project_directory, 'ffuf_dir_output.txt')
+ffuf_cmd = f"ffuf -u {url}/FUZZ -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -mc 200 -c -r -sf -o {os.path.join(project_directory, 'ffuf_dir.csv')} -of csv 2>&1 | tee {ffuf_log_path}"
+run_command(ffuf_cmd, YELLOW, "Ffuf", ffuf_log_path)
+
+# Nikto
+nikto_log_path = os.path.join(project_directory, "nikto_" + project_name)
+nikto_cmd = f"nikto -host {url} 2>&1 | tee {nikto_log_path}"
+run_command(nikto_cmd, YELLOW, "Nikto", nikto_log_path)
+
+# Subfinder
+subfinder_log_path = os.path.join(project_directory, 'subfinder_' + project_name)
+subfinder_cmd = f"subfinder -d {url} 2>&1 | tee {subfinder_log_path}"
+run_command(subfinder_cmd, YELLOW, "Subfinder", subfinder_log_path)
+
