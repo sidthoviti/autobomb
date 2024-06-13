7b445733515f4eaebc14eb30f45be6182ba26b87
Siddharth <sid@localhost.localdomain>
2024-06-13 06:50:02 +0530

    fixed whatweb, need to smooth other tools

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,18 @@
 # Use Kali Linux as the base image
-FROM kalilinux/kali-rolling
+FROM kalilinux/kali-rolling:latest
 
-# Update Packages
-RUN apt-get update -y
-
-# Install required packages
-RUN apt-get update && apt-get install -y \
-    wget \
-    curl \
-    tar \
-    make \
-    nano \
-    pv \
-    ipcalc \
-    lolcat \
-    git \
-    jq \
-    python3 \
-    python3-pip \
-    whois \
-    nmap \
-    dirsearch \
-    unzip \
-    ccze \
-    libpcap0.8-dev \
-    libuv1-dev && \
-    rm -rf /var/lib/apt/lists/*
+# Update and Install Packages
+RUN apt-get -y update \
+    && apt-get install -y \
+    apt-utils wget curl tar make dnsutils usbutils pciutils net-tools python3 python3-pip \
+    python3-setuptools python3-wheel jq whois nano vim pv ipcalc lolcat unzip ccze zip \
+    libpcap0.8-dev libuv1-dev git netcat-traditional nmap nikto ffuf gobuster impacket-scripts \
+    less locate lsof man-db smbclient smbmap socat ssh-client sslscan sqlmap telnet tmux whatweb \    
+    # Slim down layer size
+    && apt-get autoremove -y \
+    && apt-get autoclean -y \
+    # Remove apt-get cache from the layer to reduce container size
+    && rm -rf /var/lib/apt/lists/*
 
 # Install Go
 ENV GOLANG_VERSION 1.22.0
@@ -33,16 +20,40 @@ RUN wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz && \
     tar -C /usr/local -xzf go$GOLANG_VERSION.linux-amd64.tar.gz && \
     rm go$GOLANG_VERSION.linux-amd64.tar.gz
 
+# enum4linux-ng
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends python3-impacket python3-ldap3 python3-yaml \
+    && mkdir /tools \
+    # Slim down layer size
+    && apt-get autoremove -y \
+    && apt-get autoclean -y \
+    && rm -rf /var/lib/apt/lists*
+
+WORKDIR /tools
+RUN git clone https://github.com/cddmp/enum4linux-ng.git /tools/enum4linux-ng \
+    && ln -s /tools/enum4linux-ng/enum4linux-ng.py /usr/local/bin/enum4linux-ng
+
+# Install Seclists
+RUN mkdir -p /usr/share/seclists \
+    # The apt-get install seclists command isn't installing the wordlists, so clone the repo.
+    && git clone --depth 1 https://github.com/danielmiessler/SecLists.git /usr/share/seclists
+
+# Prepare rockyou wordlist
+#RUN mkdir -p /usr/share/wordlists
+#WORKDIR /usr/share/wordlists
+#RUN cp /usr/share/seclists/Passwords/Leaked-Databases/rockyou.txt.tar.gz /usr/share/wordlists/ \
+#    && tar -xzf rockyou.txt.tar.gz
+
 # Set Go environment variables
 ENV GOPATH="$HOME/go-workspace"
 ENV GOROOT="/usr/local/go"
 ENV PATH="$PATH:$GOROOT/bin/:$GOPATH/bin"
 
 # Clone the autobomb repository
-RUN git clone https://github.com/sidthoviti/autobomb.git /app/autobomb
+RUN git clone https://github.com/sidthoviti/autobomb.git /root/autobomb
 
 # Set the working directory
-WORKDIR /app/autobomb
+WORKDIR /root/autobomb
 
 # Install WebAnalyze
 RUN go install -v github.com/rverton/webanalyze/cmd/webanalyze@latest
@@ -74,12 +85,9 @@ RUN go install github.com/ffuf/ffuf/v2@latest
 # Install Subfinder
 RUN go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
-# Install Nikto
-RUN apt-get install nikto -y
-
 # Run updates for tools
 RUN webanalyze -update
 RUN nuclei -up && nuclei
 RUN subfinder -up
-RUN httpx -up
+#RUN httpx -update
 
--- autobomb.py
+++ autobomb.py
@@ -13,20 +13,10 @@ CYAN = "\033[36m"
 END = "\033[0m"
 NC = "\033[0m"  # No Color
 
-banner = f"""
-{RED}       d8888          888            888888b.                          888      
-      d88888          888            888  "88b                         888      
-     d88P888          888            888  .88P                         888      
-    d88P 888 888  888 888888 .d88b.  8888888K.   .d88b.  88888b.d88b.  88888b.  
-   d88P  888 888  888 888   d88""88b 888  "Y88b d88""88b 888 "888 "88b 888 "88b 
-  d88P   888 888  888 888   888  888 888    888 888  888 888  888  888 888  888 
- d8888888888 Y88b 888 Y88b. Y88..88P 888   d88P Y88..88P 888  888  888 888 d88P 
-d88P     888  "Y88888  "Y888 "Y88P"  8888888P"   "Y88P"  888  888  888 88888P"  
-                                                                                
-                                                                                
-                                                                                
-"""
-print(banner)
+from pyfiglet import Figlet
+custom_fig = Figlet(font='univers', width = 400)
+
+print(custom_fig.renderText('AutoSecOps'))
 print(f"\n{YELLOW}Penetration Testing Automation Framework \n{GREEN}by Siddharth Thoviti")
 print(f"\n{BLUE}Version: {CYAN}0.1b")
 print(f"\n{BLUE}Info: {CYAN}The script is still in early development stages. Please report any bugs that you may encounter.")
@@ -48,7 +38,7 @@ url = config.get("URL")
 cookie = config.get("Cookie")
 project_name = config.get("ProjectName")
 log_location = config.get("LogLocation", ".")
-wordlist_path = config.get("WordlistPath", "/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt")
+wordlist_path = config.get("WordlistPath", "/usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt")
 
 if not url:
     print("Error: URL not provided in the config file.")
@@ -69,8 +59,8 @@ def run_command(cmd, color, tool_name, log_path):
 
 # WhatWeb
 whatweb_log_path = os.path.join(project_directory, 'whatweb_' + project_name)
-webtech_cmd = f"whatweb {url} 2>&1 | tee {whatweb_log_path}"
-run_command(whatweb_cmd, YELLOW, "WhatWeb", webtech_log_path)
+whatweb_cmd = f"whatweb {url} 2>&1 | tee {whatweb_log_path}"
+run_command(whatweb_cmd, YELLOW, "WhatWeb", whatweb_log_path)
 
 # webanalyze
 webanalyze_log_path = os.path.join(project_directory, 'webanalyze_' + project_name)
@@ -99,22 +89,22 @@ nmap_cmd = f"nmap -A -T4 {url.split('://')[1]} 2>&1 | tee {nmap_log_path}"
 run_command(nmap_cmd, YELLOW, "Nmap", nmap_log_path)
 
 # Fuff Directory Fuzzing
-# Check if the URL ends with a page extension or has a trailing slash
-if url.endswith(('/', '.php', '.html', '.htm', '.asp', '.aspx', '.jsp', '.cgi')):
-    # If it ends with a page extension or has a trailing slash, remove the last part to get the directory
-    ffuf_url = url.rstrip('/') + '/'  # Remove trailing slash if present, then add a new one
-else:
-    # If it doesn't end with a page extension and doesn't have a trailing slash, remove the last part to get the directory
+# Directory Fuzzing
+ffuf_log_path = os.path.join(project_directory, 'ffuf_dir_output.txt')
+
+# Check if the URL ends with a page extension
+if url.endswith(('.php', '.html', '.htm', '.asp', '.aspx', '.jsp', '.cgi')):
+    # Remove the page extension from the URL
     url_parts = url.rsplit('/', 1)
     if len(url_parts) == 2:
-        ffuf_url = url_parts[0] + '/'  # Modify the URL for ffuf
+        ffuf_url = url_parts[0] + '/FUZZ'
     else:
-        ffuf_url = url  # Use the original URL if it doesn't have a path component
+        ffuf_url = url + '/FUZZ'
+else:
+    ffuf_url = url + '/FUZZ'
 
-ffuf_log_path = os.path.join(project_directory, 'ffuf_dir_output.txt')
-ffuf_cmd = f"ffuf -u {ffuf_url}FUZZ -w {wordlist_path} -mc all -c -r -sf -o {os.path.join(project_directory, 'ffuf_dir.csv')} -of csv 2>&1 | tee {ffuf_log_path}"
+ffuf_cmd = f"ffuf -u {ffuf_url} -w {wordlist_path} -mc all -c -r -sf -ac -o {os.path.join(project_directory, 'ffuf_dir.csv')} -of csv 2>&1 | tee {ffuf_log_path}"
 run_command(ffuf_cmd, YELLOW, "Ffuf", ffuf_log_path)
-#ffuf_cmd = f"ffuf -u {url}/FUZZ -w {wordlist_path} -mc all -c -r -sf -o {os.path.join(project_directory, 'ffuf_dir.csv')} -of csv 2>&1 | tee {ffuf_log_path}"
 
 # Nikto
 nikto_log_path = os.path.join(project_directory, "nikto_" + project_name)
